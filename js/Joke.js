f13a28ed885d65f5c46228f38fd86969896b386a
Bryan Rebong <bryan.rebong@student.ehb.be>
2023-03-22 11:56:44 +0100

    Added new JS file for classes

---
+++ js/Joke.js
@@ -0,0 +1,8 @@
+class Joke {
+    constructor(setup, delivery) {
+        this.setup = setup;
+        this.delivery = delivery;
+    }
+}
+
+const firstJoke = first joke ("How do you make holy water?", "You boil the hell out of it.");
